049ef077891a5a868884741d554eb583bd4ad440
laniley <melanie.mende@me.com>
2015-01-02 16:24:38 +0100

    search meethub button

--- public/locales/de.js
+++ public/locales/de.js
@@ -7,7 +7,8 @@ Em.I18n.translations = {
   "meethub": {
     "not-in-a-meethub": "Du bist noch nicht in einem Meethub.<br/> M&ouml;chtest du einen erstellen?",
     "button": {
-      "create": "Meethub erstellen"
+      "create": "Meethub erstellen",
+      "search": "Meethub suchen"
     }
   }
 };
--- public/locales/en.js
+++ public/locales/en.js
@@ -7,7 +7,8 @@ Em.I18n.translations = {
   "meethub": {
     "not-in-a-meethub": "You are not in a meethub yet.<br /> Do you want to create one?",
     "button": {
-      "create": "create a Meethub"
+      "create": "create a Meethub",
+      "search": "search for a Meethub"
     }
   }
 };
