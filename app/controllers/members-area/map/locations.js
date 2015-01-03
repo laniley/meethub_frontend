edb4b0d0f3f5ead3666b7566b517f8fa9d0cf03e
laniley <melanie.mende@me.com>
2015-01-03 15:24:04 +0100

    center map on location on click

---
+++ app/controllers/members-area/map/locations.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  needs: "members-area/map",
+  map_controller: Ember.computed.alias("controllers.members-area/map"),
+
+  actions: {
+    center_map: function(location) {
+      this.get('map_controller').set('lat', location.get('latitude'));
+      this.get('map_controller').set('lng', location.get('longitude'));
+    }
+  }
+});
