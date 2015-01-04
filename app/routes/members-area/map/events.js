573f17f3c835541af6bf360195b7477668ecf403
laniley <melanie.mende@me.com>
2015-01-04 19:34:40 +0100

    outlets reorganized

--- app/routes/members-area/map/events.js
+++ app/routes/members-area/map/events.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController: function(controller) {
+    controller.set('model', this.store.all('event'));
+  },
+  renderTemplate: function() {
+    this.render({ outlet: 'side-nav-bar' });
+  }
 });
--- app/routes/members-area/map/locations.js
+++ app/routes/members-area/map/locations.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.set('model', this.store.all('location'));
+  },
+  renderTemplate: function() {
+    this.render({ outlet: 'side-nav-bar' });
   }
 });
