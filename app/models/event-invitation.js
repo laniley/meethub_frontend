555bf1d51cbd62d2780ea9f532a5d13e3a5bb013
laniley <melanie.mende@me.com>
2015-01-11 21:39:04 +0100

    set button color

--- app/models/event-invitation.js
+++ app/models/event-invitation.js
@@ -4,5 +4,38 @@ export default DS.Model.extend({
   event: DS.belongsTo('event'),
   invited_user: DS.belongsTo('user'),
   message: DS.belongsTo('message'),
-  status: DS.attr('string')
+  status: DS.attr('string'),
+
+  hasBeenAccepted: function() {
+    if(this.get('status') === 'attending')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }.property('status'),
+
+  hasBeenAcceptedMaybe: function() {
+    if(this.get('status') === 'maybe')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }.property('status'),
+
+  hasBeenDeclined: function() {
+    if(this.get('status') === 'declined')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }.property('status'),
 });
