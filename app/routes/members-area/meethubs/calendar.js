cbdf61f955c6249b8c7db0156f963640bdc57f03
laniley <melanie.mende@me.com>
2015-01-24 15:02:15 +0100

    keep subsection open

--- app/routes/members-area/meethubs/calendar.js
+++ app/routes/members-area/meethubs/calendar.js
@@ -3,6 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
     this.controllerFor('members-area').set('currentSection', 'calendar');
+
+    var currentSection = this.controllerFor('members-area/meethubs').get('currentSection');
+
+    if(currentSection === 'create')
+    {
+      this.transitionTo('members-area.meethubs.calendar.create');
+    }
+    else if(currentSection === 'search')
+    {
+      this.transitionTo('members-area.meethubs.calendar.search');
+    }
   },
   setupController: function(controller) {
     controller = this.controllerFor('members-area/calendar');
