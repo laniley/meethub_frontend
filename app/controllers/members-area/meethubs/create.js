a68322df607e25705e3c39c0e78016a60fd8dabe
laniley <melanie.mende@me.com>
2015-01-25 21:08:07 +0100

    meethub creation

--- app/controllers/members-area/meethubs/create.js
+++ app/controllers/members-area/meethubs/create.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['members-area'],
+  membersArea_controller: Ember.computed.alias("controllers.members-area"),
 
   name: '',
   short_description: '',
@@ -20,6 +22,22 @@ export default Ember.Controller.extend({
     cancelActivity: function() {
       this.controllerFor('members-area/meethubs').set('currentSection', null);
       this.transitionTo('members-area.meethubs');
+    },
+
+    createMeethub: function() {
+
+      var user = this.get('membersArea_controller').get('model');
+
+      var meethub = this.store.createRecord('meethub', {
+        name: this.get('name'),
+        short_description: this.get('short_description'),
+
+        founder: user
+      });
+
+      meethub.get('members').pushObject(user);
+
+      meethub.save();
     }
   }
 });
