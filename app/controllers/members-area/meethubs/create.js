afd7ee2f629873cdcd0a9b62f09ea5636cd423a5
laniley <melanie.mende@me.com>
2015-01-25 19:12:23 +0100

    show save-button only when name is not empty

--- app/controllers/members-area/meethubs/create.js
+++ app/controllers/members-area/meethubs/create.js
@@ -1,6 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  name: '',
+  short_description: '',
+
+  nameIsNotEmpty: function() {
+    if(this.get('name.length') > 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }.property('name.length'),
+
   actions: {
     cancelActivity: function() {
       this.controllerFor('members-area/meethubs').set('currentSection', null);
