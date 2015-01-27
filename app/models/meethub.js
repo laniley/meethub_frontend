a1c74e559b2a304e1d14a1c000fac2b8d2934495
Melanie Mende <melanie.mende@tredex.de>
2015-01-27 16:33:42 +0100

    show meethub-founder

--- app/models/meethub.js
+++ app/models/meethub.js
@@ -4,7 +4,9 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   short_description: DS.attr('string'),
   isOpen: DS.attr('boolean', {defaultValue: false}),
+  created_at: DS.attr('date'),
+  updated_at: DS.attr('date'),
 
-  founder: DS.belongsTo('user'),
+  founder: DS.belongsTo('user', { async: true }),
   members: DS.hasMany('user')
 });
