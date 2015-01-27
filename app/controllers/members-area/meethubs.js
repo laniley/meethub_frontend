4fb6df836451976e3183abb204581168265c97bb
Melanie Mende <melanie.mende@tredex.de>
2015-01-27 15:07:16 +0100

    meethub can be toggled

--- app/controllers/members-area/meethubs.js
+++ app/controllers/members-area/meethubs.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  currentSection: null
+  currentSection: null,
+
+  actions: {
+    toggleMeethub: function(meethub) {
+      if(meethub.get('isOpen') === false)
+      {
+        meethub.set('isOpen', true);
+
+        if(meethub.get('hasBeenRead') === false)
+        {
+          meethub.set('hasBeenRead', true);
+          meethub.save();
+        }
+      }
+      else
+      {
+        meethub.set('isOpen', false);
+      }
+    }
+ }
 });
--- app/models/meethub.js
+++ app/models/meethub.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   short_description: DS.attr('string'),
+  isOpen: DS.attr('boolean', {defaultValue: false}),
 
   founder: DS.belongsTo('user'),
   members: DS.hasMany('user')
