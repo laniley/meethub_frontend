199c326b4c48f54fd5f1db65f0bcc7558de7ab2e
laniley <melanie.mende@me.com>
2015-01-30 12:01:17 +0100

    user gets loaded from BE

--- app/routes/members-area.js
+++ app/routes/members-area.js
@@ -40,54 +40,33 @@ export default AuthenticateRoute.extend({
       {
         console.log('Successful login to FB for: ' + response.first_name + ' ' + response.last_name, response);
 
-        var user = self.store.createRecord('user', {
-          fb_id: response.id,
-          first_name: response.first_name,
-          last_name: response.last_name,
-          picture: 'http://graph.facebook.com/' + response.id + '/picture'
+        self.store.find('user', { fb_id: response.id }).then(function(users)
+        {
+          if(Ember.isEmpty(users))
+          {
+            var user = self.store.createRecord('user', {
+              fb_id: response.id,
+              first_name: response.first_name,
+              last_name: response.last_name,
+              picture: 'http://graph.facebook.com/' + response.id + '/picture'
+            });
+
+            user.save().then
+            (
+              function()
+              {
+                self.prepareController(controller, user, response);
+              }
+            );
+          }
+          else
+          {
+            var user = users.get('firstObject');
+            self.prepareController(controller, user, response);
+          }
         });
 
-        user.save().then
-        (
-          function()
-          {
-            controller.set('model', user);
 
-            if(response.friends.data.length > 0)
-            {
-              for(var i = 0; i < response.friends.data.length; i++)
-              {
-                FB.api('/' + response.friends.data[i].id, function(friend_response)
-                {
-                  if( !friend_response.error )
-                  {
-                    console.log('friend: ', friend_response);
-
-                    self.handleFBFriendResponse(user, friend_response);
-                  }
-                  else
-                  {
-                    console.log(friend_response.error);
-                  }
-                });
-              }
-            }
-
-            // load Meethubs from BE
-            // self.store.find('meethub', { member: user.get('id') });
-            // load Messages from BE
-            // self.store.find('message', { user: user.get('id') });
-            // load EventInvitations from BE
-            // self.store.find('eventInvitation', { invited_user: user.get('id') });
-
-            self.controllerFor('members-area').loadUserEventsFromFB();
-
-            var map_controller = self.controllerFor('members-area.map');
-                map_controller.getCurrentPosition();
-
-            self.transitionTo('members-area.meethubs.map');
-          }
-        );
       }
       else
       {
@@ -95,6 +74,47 @@ export default AuthenticateRoute.extend({
       }
 
     });
+  },
+
+  prepareController: function(controller, user, response) {
+
+    var self = this;
+
+    controller.set('model', user);
+
+    // if(response.friends.data.length > 0)
+    // {
+    //   for(var i = 0; i < response.friends.data.length; i++)
+    //   {
+    //     FB.api('/' + response.friends.data[i].id, function(friend_response)
+    //     {
+    //       if( !friend_response.error )
+    //       {
+    //         console.log('friend: ', friend_response);
+
+    //         self.handleFBFriendResponse(user, friend_response);
+    //       }
+    //       else
+    //       {
+    //         console.log(friend_response.error);
+    //       }
+    //     });
+    //   }
+    // }
+
+    // load Meethubs from BE
+    // self.store.find('meethub', { member: user.get('id') });
+    // load Messages from BE
+    // self.store.find('message', { user: user.get('id') });
+    // load EventInvitations from BE
+    // self.store.find('eventInvitation', { invited_user: user.get('id') });
+
+    // self.controllerFor('members-area').loadUserEventsFromFB();
+
+    var map_controller = self.controllerFor('members-area.map');
+        map_controller.getCurrentPosition();
+
+    self.transitionTo('members-area.meethubs.map');
   },
 
   handleFBFriendResponse: function(user, response) {
