03309dcbd6bfc79c6b5b52b924b4f131a4a2f009
laniley <melanie.mende@me.com>
2015-02-01 14:09:13 +0100

    search hbs

--- app/routes/members-area/meethubs/map/search.js
+++ app/routes/members-area/meethubs/map/search.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   beforeModel: function() {
     this.controllerFor('members-area/meethubs').set('currentSection', 'search');
   },
+  renderTemplate: function() {
+    this.render('members-area.meethubs.search', {
+      outlet: 'form',
+      into: 'members-area.meethubs'
+    });
+  }
 });
