6eaf119fad25f71d403a4163e592c7ef7d9a4329
laniley <melanie.mende@me.com>
2015-02-03 19:29:44 +0100

    search-term

--- app/controllers/members-area/meethubs/search.js
+++ app/controllers/members-area/meethubs/search.js
@@ -2,10 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+<<<<<<< HEAD
 	actions: {
 	   cancelActivity: function() {
 	      this.controllerFor('members-area/meethubs').set('currentSection', null);
 	      this.transitionTo('members-area.meethubs');
 	   }
  	}
+=======
+  search_term: ''
+
+>>>>>>> search-term
 });
