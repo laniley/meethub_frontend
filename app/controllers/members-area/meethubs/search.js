5cea58965f8c93226932d075f56ab51f0ab3b04e
Melanie Mende <melanie.mende@tredex.de>
2015-02-03 14:36:25 +0100

    cancel search

---
+++ app/controllers/members-area/meethubs/search.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+	actions: {
+	   cancelActivity: function() {
+	      this.controllerFor('members-area/meethubs').set('currentSection', null);
+	      this.transitionTo('members-area.meethubs');
+	   }
+ 	}
+});
