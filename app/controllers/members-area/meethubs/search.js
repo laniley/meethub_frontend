11b7fe6c7287632a38186b5d4eea2f9609e24a7f
laniley <melanie.mende@me.com>
2015-02-06 17:37:41 +0100

    meethubs - search for a user

--- app/controllers/members-area/meethubs/search.js
+++ app/controllers/members-area/meethubs/search.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['members-area','members-area/meethubs'],
+  meethubs_controller: Ember.computed.alias("controllers.members-area/meethubs"),
 
   search_term: '',
+
+  search_results: function() {
+    var self = this;
+
+    var unreadMessages = this.get('meethubs_controller').get('model').filter(function(meethub) {
+      return meethub.get('name').indexOf(self.get('search_term')) !== -1;
+    });
+
+    return unreadMessages;
+
+  }.property('search_term','meethubs_controller.@each'),
 
 	actions: {
 	   cancelActivity: function() {
