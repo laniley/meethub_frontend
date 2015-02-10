db361dd4edf286791545a076211312038b922713
laniley <melanie.mende@me.com>
2015-02-10 19:43:35 +0100

    user search list style

--- app/components/user-search.js
+++ app/components/user-search.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  search_term: ''
+  search_term: '',
+  search_results: function() {
+    var self = this;
+
+    var filteredItems = self.get('friends').filter(function(item) {
+      return item.get('name').toLowerCase().indexOf(self.get('search_term').toLowerCase()) !== -1;
+    });
+
+    return filteredItems;
+  }.property('friends.@each', 'search_term')
 });
