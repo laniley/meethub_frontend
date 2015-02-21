0eb15b48721cbab4beacfff090a6bb8e0d675bd0
laniley <melanie.mende@me.com>
2015-02-21 09:52:08 +0100

    side-nav-item style

--- app/components/leave-button.js
+++ app/components/leave-button.js
@@ -4,5 +4,22 @@ export default Ember.Component.extend({
   classNames: ['item-option','leave-button'],
 
   user: null,
-  parent: null
+  parent: null,
+
+  actions: {
+    leave: function() {
+      var parent = this.get('parent');
+      var user = this.get('user');
+      var founder = parent.get('founder');
+
+      if(founder.get('id') === user.get('id'))
+      {
+        alert('yes');
+      }
+      else
+      {
+        alert('no');
+      }
+    }
+  }
 });
