df2b16db2d163e536ce0fc05eff0f59b1f003ad1
laniley <melanie.mende@me.com>
2015-02-24 20:03:54 +0100

    welcome page update

--- app/models/user.js
+++ app/models/user.js
@@ -7,14 +7,13 @@ export default DS.Model.extend({
   picture: DS.attr(),
   locale: DS.attr('string'),
   gender: DS.attr('string'),
+  first_login: DS.attr('boolean', { defaultValue: true }),
   created_at: DS.attr('date'),
   updated_at: DS.attr('date'),
-  isMe: DS.attr('boolean', {defaultValue: false}),
+  isMe: DS.attr('boolean', { defaultValue: false }),
   friends: DS.hasMany('user', { async: true }),
   meethubInvitations: DS.hasMany('meethub-invitation', { async: true }),
-  messages: DS.hasMany('message', {
-    inverse: 'to_user'
-  }),
+  messages: DS.hasMany('message', { inverse: 'to_user' }),
 
   name: function() {
     return this.get('first_name') + ' ' + this.get('last_name');
