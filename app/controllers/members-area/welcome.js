a15cf4ebbb497d3caaecc5080ea10bea03b5a594
laniley <melanie.mende@me.com>
2015-02-26 20:53:49 +0100

    summary item style

--- app/controllers/members-area/welcome.js
+++ app/controllers/members-area/welcome.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  number_of_new_meethub_invitations: function() {
+    var unreadMeethubInvitations = [];
+
+    if(this.get('unreadMessages.length') > 0)
+    {
+      unreadMeethubInvitations = this.get('unreadMessages').filter(function(message) {
+        return message.get('isMeethubInvitation') === true;
+      });
+    }
+
+    return unreadMeethubInvitations.get('length');
+  }.property('unreadMessages.@each'),
+
   number_of_new_event_invitations: function() {
     var unreadEventInvitations = [];
 
@@ -15,9 +28,9 @@ export default Ember.Controller.extend({
     return unreadEventInvitations.get('length');
   }.property('unreadMessages.@each'),
 
-  hasUnreadMessages: function() {
+  hasUnreadMeethubInvitations: function() {
 
-    if(this.get('unreadMessages.length') > 0)
+    if(this.get('number_of_new_meethub_invitations') > 0)
     {
       return true;
     }
@@ -26,7 +39,20 @@ export default Ember.Controller.extend({
       return false;
     }
 
-  }.property('unreadMessages.@each'),
+  }.property('number_of_new_meethub_invitations'),
+
+  hasUnreadEventInvitations: function() {
+
+    if(this.get('number_of_new_event_invitations') > 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+
+  }.property('number_of_new_event_invitations'),
 
   unreadMessages: function() {
     var unreadMessages = [];
