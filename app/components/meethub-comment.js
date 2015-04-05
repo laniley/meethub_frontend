465992c296bb70996faa1df9af1db767697efa77
laniley <melanie.mende@me.com>
2015-04-05 16:43:57 +0200

    update comment

--- app/components/meethub-comment.js
+++ app/components/meethub-comment.js
@@ -2,7 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  store: null,
   inEditMode: false,
+
+  commentIsEmpty: function() {
+    if(this.get('comment.text.length') > 0)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }.property('comment.text.length'),
 
   actions: {
     editComment: function(comment) {
@@ -11,6 +23,15 @@ export default Ember.Component.extend({
     cancelEditing: function(comment) {
       comment.rollback();
       this.set('inEditMode', false);
+    },
+    updateComment: function(comment) {
+
+      if(!this.get('commentIsEmpty'))
+      {
+        comment.save();
+
+        this.set('comment', '');
+      }
     },
     deleteComment: function(comment) {
       console.log('delete');
