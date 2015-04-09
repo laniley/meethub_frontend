4e4812292107ec7712ea6c88112e01c03836dbde
laniley <melanie.mende@me.com>
2015-04-09 22:31:38 +0200

    style-fixes

--- app/controllers/members-area/meethubs/calendar.js
+++ app/controllers/members-area/meethubs/calendar.js
@@ -4,14 +4,8 @@ export default Ember.Controller.extend({
   needs: ['members-area'],
   membersArea_controller: Ember.computed.alias("controllers.members-area"),
 
-  sortProperties: ['created_at:desc'],
-  sortedComments: Ember.computed.sort('comments', 'sortProperties'),
-
-  comments: function() {
-    return this.get('model');
-  }.property('model.@each'),
-
   isSidebarOpen: function() {
     return this.get('membersArea_controller').get('isSidebarOpen');
   }.property('membersArea_controller.isSidebarOpen')
+
 });
--- app/controllers/members-area/meethubs/index.js
+++ app/controllers/members-area/meethubs/index.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['members-area'],
+  membersArea_controller: Ember.computed.alias("controllers.members-area"),
+
+  isSidebarOpen: function() {
+    return this.get('membersArea_controller').get('isSidebarOpen');
+  }.property('membersArea_controller.isSidebarOpen'),
 
   newMeethubComments: function() {
 
