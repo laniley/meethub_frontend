47203bffa8809fbea56f7e8dfa5aa202d2edccc2
laniley <melanie.mende@me.com>
2015-05-14 18:23:04 +0200

    timezone support

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export function formattedDate(date, format) {
-  return moment(date).locale(Ember.I18n.locale).format(format);
+  var offset = new Date().getTimezoneOffset() * -1;
+  return moment(date)
+          .add(offset, 'm')
+          .locale(Ember.I18n.locale)
+          .format(format);
 }
 
 export default Ember.Handlebars.makeBoundHelper(formattedDate);
