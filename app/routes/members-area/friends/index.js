9f0309fa686d6fd0f705eafcf0907afd4b7693e4
laniley <melanie.mende@me.com>
2015-05-20 21:35:57 +0200

    friends route cleaned up

--- app/routes/members-area/friends/index.js
+++ app/routes/members-area/friends/index.js
@@ -6,17 +6,6 @@ export default Ember.Route.extend({
     var user = membersAreaController.get('model');
     controller.set('model', user.get('friends'));
 
-    var currentSection = this.controllerFor('members-area').get('currentSection');
-
-    if(currentSection === 'calendar')
-    {
-      this.transitionTo('members-area.friends.calendar');
-    }
-    else
-    {
-      this.transitionTo('members-area.friends.map');
-    }
-
     this.controllerFor('members-area').set('showSidebar', true);
   },
   renderTemplate: function() {
