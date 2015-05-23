d973ea02cd9818b285d6da51588354118e236163
laniley <melanie.mende@me.com>
2015-05-23 12:43:34 +0200

    friend model

---
+++ app/models/friend.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+import User from './user';
+
+export default User.extend({
+  me: DS.belongsTo('user'),
+
+  is_a_new_friend: function() {
+    if(this.get('created_at') > this.get('logged_in_user.last_login'))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }.property('created_at', 'logged_in_user.last_login')
+});
