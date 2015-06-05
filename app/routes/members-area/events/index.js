36eff2350273580ac47804fb9912059349252d12
laniley <melanie.mende@me.com>
2015-06-05 13:18:46 +0200

    new events counter top nav bar

--- app/routes/members-area/events/index.js
+++ app/routes/members-area/events/index.js
@@ -3,28 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.set('model', this.store.all('event'));
-
-    var currentSection = this.controllerFor('members-area').get('currentSection');
-
-    if(currentSection === 'calendar')
-    {
-      this.transitionTo('members-area.events.calendar');
-    }
-    else
-    {
-      this.transitionTo('members-area.events.map');
-    }
-
     this.controllerFor('members-area').set('showSidebar', true);
   },
-  renderTemplate: function() {
-    this.render('members-area.events.side-nav-buttons', {
-      into: 'members-area',
-      outlet: 'side-nav-buttons'
-    });
-    this.render('members-area.events', {
-      into: 'members-area',
-      outlet: 'side-nav-bar'
-    });
-  }
+  // renderTemplate: function() {
+  //   this.render('members-area.events.side-nav-buttons', {
+  //     into: 'members-area',
+  //     outlet: 'side-nav-buttons'
+  //   });
+  //   this.render('members-area.events', {
+  //     into: 'members-area',
+  //     outlet: 'side-nav-bar'
+  //   });
+  // }
 });
