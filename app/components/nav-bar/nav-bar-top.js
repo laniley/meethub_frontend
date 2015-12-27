f47f4f80bb1c635b7bd6ae1c95613278a1ca96c6
laniley <melanie.mende@me.com>
2015-12-27 15:10:04 +0100

    faster logout

--- app/components/nav-bar/nav-bar-top.js
+++ app/components/nav-bar/nav-bar-top.js
@@ -7,13 +7,12 @@ export default Ember.Component.extend({
 
   actions: {
     logout() {
-      var self = this;
       var me = this.store.peekRecord('me', 1);
       me.set('isLoggedIn', false);
       FB.api( 'me/permissions', 'DELETE', function(response) {
         console.log(response);
-        self.router.transitionTo('login');
       });
+      this.router.transitionTo('login');
     }
   }
 });
